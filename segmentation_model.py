a287fd39c9ef1bec5ec5313d52d813cd45252f9c
Tensor <narutokelechi@gmail.com>
2024-05-07 12:43:09 +0100

    Unet model architecture complete

--- segmentation_model.py
+++ segmentation_model.py
@@ -1,6 +1,7 @@
+from networkx import is_planar
 import torch
 import torchvision
-import torch.nn as nn
+from torch import nn, relu
 
 # 221029255650
 
@@ -13,11 +14,10 @@ class encoder_conv_block(nn.Module):
         super().__init__()
         self.conv_block = nn.Sequential(
             nn.Conv2d(input, output, kernel_size=3, padding=1),
-            # nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(output, output, kernel_size=3, padding=1),
             nn.BatchNorm2d(output),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
         )
 
     def forward(self, x):
@@ -26,62 +26,8 @@ class encoder_conv_block(nn.Module):
         return x
 
 
-class Encoder(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # self.encode_step1 = nn.Sequential(
-        #     encoder_conv_block(3, 64),
-        #     encoder_conv_block(64, 128),
-        #     encoder_conv_block(128, 256),
-        #     encoder_conv_block(256, 512),
-        # )
-        self.ec1 = encoder_conv_block(3, 64)
-        self.ec2 = encoder_conv_block(64, 128)
-        self.ec3 = encoder_conv_block(128, 256)
-        self.ec4 = encoder_conv_block(256, 512)
-
-        self.encode_step_without_pool = nn.Sequential(
-            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
-            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
-        )
-
-    def forward(self, x):
-        x = self.ec1(x)
-        x = self.ec2(x)
-        x = self.ec3(x)
-        x = self.ec4(x)
-        x = self.encode_step_without_pool(x)
-        x = nn.ReLU(x)
-
-        return x
-
-
-# class Decoder(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.conv_transpose1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-#         self.dec1_1 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-#         self.dec1_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        
-#         self.conv_transpose2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-#         self.d2_1 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-#         self.d2_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-
-#         self.conv_transpose3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-#         self.d3_1 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-#         self.d3_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-
-#         self.conv_transpose4= nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-#         self.d4_1 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-#         self.d4_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-
-#     def forward(self, x):
-        
-#         return x
-
-
-class Unet_Hikari(nn.Module):
-    def __init__(self, n_classes):
+class Unet_model(nn.Module):
+    def __init__(self, n_classes=10):
         super().__init__()
         # Encoder
         # self.encoder = Encoder()
@@ -89,42 +35,79 @@ class Unet_Hikari(nn.Module):
         self.ec2 = encoder_conv_block(64, 128)
         self.ec3 = encoder_conv_block(128, 256)
         self.ec4 = encoder_conv_block(256, 512)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.encode_step_without_pool = nn.Sequential(
             nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
             nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
         )
 
         # Decoder
         # self.decoder = Decoder()
+
         self.conv_transpose1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.dec1_1 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-        self.dec1_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.dec1 = nn.Sequential(
+            nn.Conv2d(1024, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
         self.conv_transpose2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.d2_1 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.d2_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.dec2 = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
         self.conv_transpose3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.d3_1 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.d3_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.dec3 = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
         self.conv_transpose4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.d4_1 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.d4_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.dec4 = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
 
         self.out_conv = nn.Conv2d(64, n_classes, kernel_size=1)
 
     def forward(self, x):
-        
-        x1 = self.ec2(self.ec1(x))
-        x2 = self.ec4(self.ec3(x1))
-        xe3 = self.encode_step_without_pool(x2)
-        
-        
-        
+        xe1 = self.ec1(x)
+        xe2 = self.ec2(xe1)
+        xe3 = self.ec4(xe2)
+        xe4 = self.ec3(xe3)
 
-        return x
+        xe5 = self.encode_step_without_pool(xe4)
+
+        xd1 = self.conv_transpose1(xe5)
+        xd1 = torch.cat([xd1, xe4], dim=1)
+        xd1 = self.dec1(xd1)
+
+        xd2 = self.conv_transpose2(xd1)
+        xd2 = torch.cat([xd2, xe3], dim=1)
+        xd2 = self.dec2(xd2)
+
+        xd3 = self.conv_transpose3(xd2)
+        xd3 = torch.cat([xd3, xe2], dim=1)
+        xd3 = self.dec3(xd3)
+
+        xd4 = self.conv_transpose4(xd3)
+        xd4 = torch.cat([xd4,xe1], dim=1)
+        xd4 = self.dec4(xd4)
+        
+        output = self.out_conv(xd4)
+
+        return output
 
 
-hikari_segmenter = Unet_Hikari(n_classes=10).to(device)
+hikari_segmenter_model = Unet_model().to(device)
